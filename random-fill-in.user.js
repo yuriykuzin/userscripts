48c0a2ba7591667273eed10ce6e21dcbc54d4a15
Stepan Suvorov <stevermeister@gmail.com>
2015-07-21 22:19:14 +0200

    added dispostable option

--- random-fill-in.user.js
+++ random-fill-in.user.js
@@ -15,23 +15,34 @@
 var load,execute,loadAndExecute;load=function(a,b,c){var d;d=document.createElement("script"),d.setAttribute("src",a),b!=null&&d.addEventListener("load",b),c!=null&&d.addEventListener("error",c),document.body.appendChild(d);return d},execute=function(a){var b,c;typeof a=="function"?b="("+a+")();":b=a,c=document.createElement("script"),c.textContent=b,document.body.appendChild(c);return c},loadAndExecute=function(a,b){return load(a,function(){return execute(b)})};
 
 loadAndExecute("//rawgit.com/Marak/faker.js/master/build/build/faker.min.js", function () {
-
+  var emailName = '';
   document.addEventListener('keypress', function (event) {
     if (event.altKey && event.keyCode === 174) {
       var app = angular.element('[ng-controller="SignupController"]').scope(),
         userData = app.userData;
       userData.firstName = faker.name.firstName();
       userData.lastName = faker.name.lastName();
-      var emailName = faker.internet.email().split('@')[0];
+      emailName = faker.internet.email().split('@')[0];
       userData.email = userData.password = emailName + '@dispostable.com';
       app.$apply();
-      window.open('http://www.dispostable.com/inbox/' + emailName + '/', '_blank');
+    }
+
+    if(event.altKey && event.keyCode === 8706){
+      var currentUser = angular.element(document).injector().get('CurrentUserService').get();
+      if(currentUser && currentUser.email){
+        if(currentUser.email.split('@')[1] !== 'dispostable.com'){
+          alert('Current User has NOT dispostable email account');
+          return ;
+        }
+        emailName = currentUser.email.split('@')[0];
+      }
+
+      if(emailName){
+        window.open('http://www.dispostable.com/inbox/' + emailName + '/', '_blank');
+      }
     }
   }, false);
 });
-
-
-
 
 
 
