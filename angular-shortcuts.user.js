0175084c2c16472193cef6629ce8faa01246ad09
Stepan Suvorov <stevermeister@gmail.com>
2015-08-19 14:03:23 +0200

    updated angulary

--- angular-shortcuts.user.js
+++ angular-shortcuts.user.js
@@ -5,27 +5,37 @@
 // @description  to make console make angular friendly with shortcuts like $injector(), $getService('name'), $rootScope, $getService(element)
 // @author       Stepan Suvorov <stevermeister@gmail.com>
 // @match        *://*/*
+// @grant        none
+// @run-at	     document-start
 // ==/UserScript==
 
-
-var initWatcher = setInterval(function () {
-    console.log('watch');
-    if (unsafeWindow.angular) {
-        clearInterval(initWatcher);
-        init(unsafeWindow.angular);
-    }
-}, 100);
-
-function init(angular) {
-    console.log('angular', angular);
-    alert('inA')
-    app = window.angular.element(document);
-    $injector = app.injector;
-    $getService = $injector.get;
-    $rootScope = app.scope();
-    $getService = function(element) {
-      return window.angular.element(element).scope();
-    }
+if(document.querySelector('[src*="angular"]')) {
+  init();
 }
 
+function init() {
+  var w = typeof unsafeWindow == 'undefined' ? window : unsafeWindow,
+    tryNumber = 20,
+    initWatcher = setInterval(function() {
+      var appElement = w.document.querySelector('.ng-scope');
+      if (appElement || tryNumber) {
+        clearInterval(initWatcher);
 
+        if (appElement) {
+          initAngulary(appElement);
+        }
+      }
+      tryNumber--;
+    }, 500);
+
+  function initAngulary(el) {
+    w.$reload = w.angular.reloadWithDebugInfo;
+    w.$app = w.angular.element(el);
+    w.$injector = w.$app.injector();
+    w.$get = w.$injector.get;
+    w.$rootScope = w.$app.scope();
+    w.$scope = function(element) {
+      return w.angular.element(element).scope();
+    };
+  }
+}
