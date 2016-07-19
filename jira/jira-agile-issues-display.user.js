37bb9fdafdb8b727e1b5fa7616174d31015cf4a4
Stepan Suvorov <stevermeister@gmail.com>
2016-07-19 13:56:18 +0200

    Update jira-agile-issues-display.user.js

--- jira/jira-agile-issues-display.user.js
+++ jira/jira-agile-issues-display.user.js
@@ -30,7 +30,7 @@
 
     setTimeout(function() {
       setAvatars();
-    }, 3000);
+    }, 1000);
 
     function getData() {
       return jQuery.get('/rest/greenhopper/1.0/xboard/work/allData.json?rapidViewId=3');
